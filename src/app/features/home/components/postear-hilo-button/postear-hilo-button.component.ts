67542f63d0bf86091adbe4caea54e2662150f597
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-05-09 19:30:52 -0300

    agregado botón para postear hilo con diálogo asociado

--- src/app/features/home/components/postear-hilo-button/postear-hilo-button.component.ts
+++ src/app/features/home/components/postear-hilo-button/postear-hilo-button.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
+import { PostearHiloDialogComponent } from "../../../hilos/components/postear-hilo-dialog/postear-hilo-dialog.component";
 
 @Component({
   selector: 'home-postear-hilo-button',
-  imports: [],
+  imports: [PostearHiloDialogComponent],
   templateUrl: './postear-hilo-button.component.html',
   styleUrl: './postear-hilo-button.component.css',
 })
-export class PostearHiloButtonComponent { }
+export class PostearHiloButtonComponent {
+  visible = signal<boolean>(false);
+}
