a503ce71244d79c8514f7937307de376b1e5b948
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-05-18 19:23:59 -0300

    agregado filtros para hilos

---
+++ src/app/features/application/components/filtros-de-hilo/filtros-de-hilo.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import {Dialog} from '@angular/cdk/dialog';
+import { SeleccionarSubcategoriaDialogComponent } from '../../../categorias/components/seleccionar-subcategoria-dialog/seleccionar-subcategoria-dialog.component';
+import { Subcategoria } from '../../../categorias/interfaces/categoria.interface';
+
+
+@Component({
+  selector: 'app-filtros-de-hilo',
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  templateUrl: './filtros-de-hilo.component.html',
+  styleUrl: './filtros-de-hilo.component.css',
+})
+export class FiltrosDeHiloComponent  implements OnInit{
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    titulo : this.fb.control<string>(""),
+    subcategoria : this.fb.control<Subcategoria | undefined>(undefined),
+  })
+
+  activatedRoute = inject(ActivatedRoute);
+
+  dialog = inject(Dialog)
+
+  ngOnInit(): void {
+    let titulo  =  this.activatedRoute.snapshot.queryParams["titulo"]?? ""
+
+    this.form.patchValue({
+      titulo: titulo
+    });
+  }
+
+  mostrarSubcategorias ():void {
+    SeleccionarSubcategoriaDialogComponent.show(this.dialog, {
+      onSubcategoriaSeleccionada: (subcategoria) =>{
+        this.form.patchValue({
+          subcategoria: subcategoria
+        })
+      }
+    });
+  }
+
+  router = inject(Router);
+
+  filtrarHilos(){
+    let titulo = this.form.value.titulo != "" ? this.form.value.titulo : undefined;
+
+    let subcategoria = this.form.value.subcategoria?.id
+
+    this.router.navigate(
+      [""],
+      {
+        queryParams: 
+          { 
+            titulo,
+            subcategoria
+          } 
+      }
+    )
+  }
+}
--- src/app/features/application/components/header/header.component.ts
+++ src/app/features/application/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MenuButtonComponent } from "./components/menu-button/menu-button.component";
+import { FiltrosDeHiloComponent } from "../filtros-de-hilo/filtros-de-hilo.component";
+import { RouterModule } from '@angular/router';
+import {Overlay, OverlayModule} from '@angular/cdk/overlay';
+import { CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { NotificacionesButtonComponent } from "./components/notificaciones-button/notificaciones-button.component";
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MenuButtonComponent],
+  imports: [MenuButtonComponent, FiltrosDeHiloComponent, RouterModule, OverlayModule, PortalModule, NotificacionesButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  auth = inject(AuthService);
+  
+}
