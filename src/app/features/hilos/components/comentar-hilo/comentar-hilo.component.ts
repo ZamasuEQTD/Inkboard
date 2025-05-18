3deb6c28faaa24422ea1d622e325eec1ddfa30d3
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-05-18 19:31:19 -0300

    agregado implmentacion para enviar comentario y responsive

--- src/app/features/hilos/components/comentar-hilo/comentar-hilo.component.ts
+++ src/app/features/hilos/components/comentar-hilo/comentar-hilo.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { PickedMedia } from '../../../../shared/interfaces/picked-media.interface';
 import { PickFileComponent } from "../../../../shared/components/pick-file/pick-file.component";
 import { PickedMediaThumbnailComponent } from "../../../../shared/components/picked-media-thumbnail/picked-media-thumbnail.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'comentar-hilo',
@@ -16,6 +17,8 @@ export class ComentarHiloComponent {
   hiloPageService = inject(HiloPageService);
 
   form = this.hiloPageService.comentarHiloForm;
+
+  http = inject(HttpClient)
 
   get textoControl(): FormControl {
     return this.form.get('texto') as FormControl;
@@ -42,5 +45,28 @@ export class ComentarHiloComponent {
     this.form.patchValue({
       files: [...archivos],
     });
+  }
+
+  enviar() :void {
+    var data = new FormData();
+
+    data.append('texto', this.form.value.texto!);
+
+    const files = this.form.value.files!;
+
+    if(files.length !== 0){
+      const file = files[0];
+
+      data.append('file', file.file!);
+      
+      data.append('spoiler', file.spoiler!.toString())
+    }
+
+    this.http.post<void>(`/api/comentarios/comentar-hilo/${this.hiloPageService.hilo()!.id}`,data).subscribe(()=> {
+      this.hiloPageService.comentarHiloForm.reset({
+        files : [],
+        texto :""
+      })
+    })
   }
 }
